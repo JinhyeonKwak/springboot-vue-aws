00f8ac459935d56c3d27404b9a34238799c07aae
jinhyeon <vmflwu123@gmail.com>
2023-03-05 16:06:22 +0900

    헤더 인증으로 변경, 그리고 Intellij http

--- src/main/java/com/hodolog/config/data/UserSession.java
+++ src/main/java/com/hodolog/config/data/UserSession.java
@@ -1,7 +1,13 @@
 package com.hodolog.config.data;
 
+import lombok.Getter;
+
+@Getter
 public class UserSession {
 
-    public String name;
+    private final Long id;
 
+    public UserSession(Long id) {
+        this.id = id;
+    }
 }
