79a907171d777a8a47a595c146c340bb1c197d58
jinhyeon <vmflwu123@gmail.com>
2023-03-24 11:44:26 +0900

    쿠키를 통한 인증 및 검증

--- src/main/java/com/hodolog/controller/AuthController.java
+++ src/main/java/com/hodolog/controller/AuthController.java
@@ -1,13 +1,18 @@
 package com.hodolog.controller;
 
+import com.google.common.net.HttpHeaders;
 import com.hodolog.request.Login;
 import com.hodolog.response.SessionResponse;
 import com.hodolog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -17,7 +22,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
-        return new SessionResponse(authService.signIn(login));
+    public ResponseEntity<Object> login(@RequestBody Login login) {
+        String accessToken = authService.signIn(login);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost")
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+        log.info(">>>>>>>>>>>> cookie={}", cookie.toString());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
